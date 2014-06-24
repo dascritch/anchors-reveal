8f1cbe1524d195286bb9666971ee8f5126f845db
dascritch <xavier@dascritch.com>
2014-06-24 11:54:25 +0200

    Écriture d'un layout

--- README.md
+++ README.md
@@ -1,6 +1,53 @@
-TODO
+ANCHORS REVEAL
+==============
 
+Addon to show anchors in a web page
+
+Author :  [Xavier "dascritch" Mouton-Dubosc](http://dascritch.com)
+Icon taken from the default KDE theme
+
+Version : 0.1
+
+
+
+TODO
+----
+* bug positionnement offset
 * Documentation
 * Purposes
 * Do it as a Firefox addon
-* Creating a layer including style scoped
+* Original logo in SVG
+* Refresh on resize
+* Refresh on mutations
+
+Licence
+-------
+
+Copyright (C) 2014 Xavier "dascritch" Mouton-Dubosc
+
+This software is licenced under the [GNU General Purpose Licence](http://www.gnu.org/licenses/gpl-3.0.txt).
+Use it and deploy it as you want : i've done too much closed source before.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Versions
+--------
+* June 2014 : 0.1 , first version. Standalone JS and Firefox Addon
+
+Repository : <https://github.com/dascritch/anchors-reveal>
+
+Keeping in touch :
+* professional <http://dascritch.com>
+* blog <http://dascritch.net>
+* twitter : [@dascritch](https://twitter.com/dascritch)
--- data/anchors-reveal.js
+++ data/anchors-reveal.js
@@ -1,37 +1,61 @@
 'use strict';
 
-var prefix = 'anchors-reveal';
-
-if (document.getElementById(prefix+'-style') === null) {
+var prefix = 'dascritch---anchors-reveal';
+function reBuild() {
 	document.body.insertAdjacentHTML('beforeend',
-		'<style id="'+prefix+'-style">'+
-			'.'+prefix+' {'+
-				' position : ABSOLUTE ;'+
-				' font-family : sans-serif ;'+
-				' font-size : 14px ;'+
-				' font-weight : bold;'+
-				' background : yellow ;'+
-				' color : black;'+
-				' padding : 4px ;'+
-				' border : 1px black solid ;'+
-				' opacity : 0.7 ;'+
-			'}'+
-			'.'+prefix+':hover {'+
-				' opacity : 1 ;'+
-				' color : black;'+
-			'}'+
-		'</style>'
+		'<div id="'+prefix+'" style="'+
+			'position : static;'+
+			'top : 0px;'+
+			'bottom : 0px;'+
+			'left : 0px;'+
+			'right : 0px;'+
+			'margin : 0px;'+
+			'padding : 0px;'+
+			'border : none;'+
+			'width : 100%;'+
+			'height : 100%;'+
+			'overflow : hidden;'+
+			'z-index : 50;'+
+			'pointer-events : none;'+
+			'">'+
+			'<style scoped>'+
+				'a'+' {'+
+					' position : absolute ;'+
+					' font-family : sans-serif ;'+
+					' font-size : 14px ;'+
+					' font-weight : bold;'+
+					' background : yellow ;'+
+					' color : black;'+
+					' padding : 4px ;'+
+					' border : 1px black solid ;'+
+					' opacity : 0.7 ;'+
+					'pointer-events : auto;'+
+				'}'+
+				'a:hover {'+
+					' opacity : 1 ;'+
+					' color : black;'+
+				'}'+
+			'</style>'+
+		'</div>'
+	);
+	var layout = document.getElementById(prefix);
+	[].forEach.call(document.querySelectorAll('[id]'),
+	    function(el) {
+	    	if (el.id.indexOf(prefix)!==0) {
+	        	layout.insertAdjacentHTML('beforeend',
+	        		'<a href="#'+el.id+'" style="top:'+el.offsetTop+'px;left:'+el.offsetLeft+'px;">#'+el.id+'</a>'
+	        	);
+	        }
+		}
 	);
 }
 
-if (document.querySelector('.'+prefix) === null) {
-	[].forEach.call(document.querySelectorAll('[id]'),
-        function(el){
-        	if (el.id.indexOf(prefix)!==0) {
-	        	document.body.insertAdjacentHTML('beforeend',
-	        		'<a class="'+prefix+'" href="#'+el.id+'" style="top:'+el.offsetTop+'px;left:'+el.offsetLeft+'px;">#'+el.id+'</a>'
-	        	);
-	        }
-    	}
-    );
+function destroy() {
+	document.body.removeChild(document.getElementById(prefix));
+}
+
+if (document.getElementById(prefix) === null) {
+	reBuild();
+} else {
+	destroy();
 }

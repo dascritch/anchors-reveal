8c0a9334aaa20a0b635d4e19c785446a2415854e
dascritch <xaviermd@gmail.com>
2017-10-03 18:05:10 +0200

    Cleanse and add menu

--- background.js
+++ background.js
@@ -1,29 +1,38 @@
+var script_to_call = {file:'/data/anchors-reveal.js'};
+var menu_id = 'anchors-reveal';
+
 chrome.browserAction.onClicked.addListener(function(tab){
-    chrome.tabs.executeScript(tab.id, {file:'/data/anchors-reveal.js'}, handleExecuteScriptAndInsertCSSErrors);
+    chrome.tabs.executeScript(tab.id, script_to_call, handleExecuteScriptAndInsertCSSErrors);
 });
 
 //The following was modified from http://stackoverflow.com/a/40517692/3773011 Copied by the author of the post.
 function handleExecuteScriptAndInsertCSSErrors(tabId){
     if(chrome.runtime.lastError){
-        var message = chrome.runtime.lastError.message;
         var isFirefox = !!window.InstallTrigger;
-        var extraMessage = 'on this URL.';
+
+        var message = chrome.runtime.lastError.message ? chrome.runtime.lastError : chrome.runtime.lastError.message;
+
+        // REALLY UGLY
         if((!isFirefox && message.indexOf('Cannot access a chrome:') > -1) //Chrome
             || (isFirefox && message.indexOf('No window matching') > -1) //Firefox
         ){
             //The current tab is one into which we are not allowed to inject scripts.
-            var messageText= 'This extension, ' + chrome.runtime.id
-                             + ', does not work ' + extraMessage;
-            console.log(messageText);
+            console.info('The anchors-reveal extention does not work on this URL.');
         } else {
             // Report the error
-            if(isFirefox){
-                //In Firefox, runtime.lastError is an Error Object including a stack trace.
-                console.error(chrome.runtime.lastError);
-            }else{
-                console.error(message);
-            }
+            console.error(message);
         }
     }
 }
 
+
+browser.contextMenus.create({
+  id: menu_id,
+  title: browser.i18n.getMessage('buttonDescription')
+});
+
+browser.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === menu_id) {
+    browser.tabs.executeScript(script_to_call);
+  }
+});

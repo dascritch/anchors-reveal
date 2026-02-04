5ade382e1cee436423093bdf7547e329c893bd4b
Xavier Mouton-Dubosc <xavier@dascritch.com>
2026-02-04 11:41:04 +0100

    refacto: simplify background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-import { menu_id, THEMES, default_theme } from './lib.js';
+import { menu_id, THEMES, default_theme, listener, install_event_act, install_context_menu } from './lib.js';
 import { switch_layer } from './anchors-reveal.js';
 
 if (typeof browser === 'undefined') {
@@ -8,74 +8,10 @@ if (typeof browser === 'undefined') {
 }
 
 
-let target_tab_id ;
-
-/** TODO
- * remettre le menu switch off
- * passer une partie du style en variable pour la remonter en lib.js
- * passer en ws
- * https://github.com/mozilla/webextension-polyfill
-**/
-
-function feedback(output) {
-
-	if ((output.length < 1) || (output[0] === undefined)) {
-		throw Error(`Anchors-reveal had an issue : ${output}`);
-		return;
-	}
-	const {result, error} = output[0];
-
-	if (error) {
-		throw Error(`Anchors-reveal had an error : ${error}`);
-		return
-	}
-
-	let theme = THEMES[result.theme ?? default_theme];
-	browser.action.setBadgeText({ text:
-		result.displayed ? String(result.count_tags) : null
-	});
-	browser.action.setBadgeBackgroundColor({ color:theme.background });
-	browser.action.setBadgeTextColor({ color:theme.color });
-}
-
-function denied_action(e) {
-	console.error(`Anchors-reveal action is not available here: ${e}`)
-	// may be trigerred by a security feature
-	browser.action.setBadgeBackgroundColor({ color:'transparent' });
-	browser.action.setBadgeTextColor({ color:'red' });
-	browser.action.setBadgeText({ text: '⛔'});
-}
-
-function listener(tab, _) {
-	target_tab_id = tab.id;
-	browser.scripting.executeScript({
-		func    : switch_layer,
-		target	: { tabId: target_tab_id },
-		world	: 'ISOLATED'
-	}).then(feedback).catch(denied_action);
-}
-
-function menu_listener(_, tab) {
-	// only one menu entry, no need to check
-	listener(tab);
-}
-
-function install_event_act(event, act) {
-	if (event.hasListener(act)) {
-		event.removeListener(act)
-	}
-	event.addListener(act);
-}
-
 async function on_installed() {
 	install_event_act(browser.action.onClicked, listener);
-	browser.contextMenus.remove(menu_id).catch(e => {});
-
-	browser.contextMenus.create({
-		id: menu_id,
-		title: browser.i18n.getMessage('buttonDescription'),
-	});
-	install_event_act(browser.contextMenus.onClicked, menu_listener);
+	install_context_menu();
+	
 }
 
 on_installed();
